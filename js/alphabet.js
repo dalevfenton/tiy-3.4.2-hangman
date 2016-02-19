d7052751a3b368dbecbf3610a712ad6bc15dcf44
Dale Fenton <fentondale@gmail.com>
2016-02-19 12:35:54 -0500

    keeps track of guesses and can be reset on win or loss

---
+++ js/alphabet.js
@@ -0,0 +1,28 @@
+function Alphabet(){
+  this.a = {guessed:false, correct:false};
+  this.b = {guessed:false, correct:false};
+  this.c = {guessed:false, correct:false};
+  this.d = {guessed:false, correct:false};
+  this.e = {guessed:false, correct:false};
+  this.f = {guessed:false, correct:false};
+  this.g = {guessed:false, correct:false};
+  this.h = {guessed:false, correct:false};
+  this.i = {guessed:false, correct:false};
+  this.j = {guessed:false, correct:false};
+  this.k = {guessed:false, correct:false};
+  this.l = {guessed:false, correct:false};
+  this.m = {guessed:false, correct:false};
+  this.n = {guessed:false, correct:false};
+  this.o = {guessed:false, correct:false};
+  this.p = {guessed:false, correct:false};
+  this.q = {guessed:false, correct:false};
+  this.r = {guessed:false, correct:false};
+  this.s = {guessed:false, correct:false};
+  this.t = {guessed:false, correct:false};
+  this.u = {guessed:false, correct:false};
+  this.v = {guessed:false, correct:false};
+  this.w = {guessed:false, correct:false};
+  this.x = {guessed:false, correct:false};
+  this.y = {guessed:false, correct:false};
+  this.z = {guessed:false, correct:false};
+}
--- js/main.js
+++ js/main.js
@@ -4,31 +4,39 @@
 //------------------------------------------------------------------------------
 //                       INTITIALIZE APPLICATION STATE
 //------------------------------------------------------------------------------
-  //filter list of commonWords to down to those with 4 letters or more
+  //filter list of commonWords to down to those with 3 letters or more
   var usefulWords = commonWords.filter(function(value){
-    if(value.length > 3){
+    if(value.length >= 3){
       return true;
     }else{
       return false;
     }
   });
   //get a random word from our list of usable words
-  var randomWord = _.sample(usefulWords);
-  console.log(randomWord);
+  var randomWord;
+  var alphabetObj = new Alphabet();
   var gameBoard = document.querySelector('.game-board');
   var indicator = document.querySelector('.indicator');
+  var guessLetters = document.querySelector('.guessed-letters');
+  var winScreen = document.querySelector('.winner');
+  var loseScreen = document.querySelector('.loser');
   var numGuesses = 10;
-  var guesses = [];
-  var boardDisplay = {};
-  // _.each(randomWord, function(element, index, list){
-  //   boardDisplay[element] =
-  // });
-  updateIndicator();
-  outputToBoard(randomWord, guesses);
-  function outputToBoard( randomWord, guesses ){
+
+  function initializeGame(){
+    randomWord = _.sample(usefulWords);
+    alphabetObj = new Alphabet();
+    numGuesses = 10;
+    outputToBoard();
+    updateIndicator();
+    winScreen.style.top = '-50%';
+    loseScreen.style.top = '-50%';
+    console.log(alphabetObj);
+    console.log(randomWord);
+  }
+  function outputToBoard(  ){
     var outputStr = '';
     _.each( randomWord, function(value, indexVal, arr){
-      if(guesses.indexOf(value) > -1 ){
+      if(alphabetObj[value].correct === true ){
         outputStr += '<span class="letter letter-' + indexVal + '">' + value + '</span>';
       }else{
         outputStr += '<span class="letter letter-' + indexVal + '"></span>';
@@ -38,18 +46,61 @@
   }
   function updateIndicator(){
     indicator.innerHTML = 'Number of Guesses Left: ' + numGuesses;
+    alphaOutput = '';
+    _.each(alphabetObj, function(value, indexVal, arr){
+      if(value.guessed === true){
+        if(value.correct === true){
+          alphaOutput += '<span class="letter letter-correct">' + indexVal + '</span>';
+        }else{
+          alphaOutput += '<span class="letter letter-wrong">' + indexVal + '</span>';
+        }
+      }else{
+        alphaOutput += '<span class="letter"></span>';
+      }
+    });
+    guessLetters.innerHTML = alphaOutput;
+  }
+  function checkWin(){
+    var win = true;
+    _.each(randomWord, function(value){
+      if(alphabetObj[value].correct === false){
+        win = false;
+      }
+    });
+    console.log(win);
+    return win;
+  }
+  function checkLose(){
+    var lose = false;
+    if(numGuesses === 0){
+      lose = true;
+    }
+    return lose;
   }
   function handleKey (){
     if(event.code.slice(0,3) == "Key"){
+      var key = event.code.slice(3,4).toLowerCase();
       console.log('key is: ' + event.code.slice(3,4));
-      guesses.push(event.code.slice(3,4).toLowerCase());
-      console.log(guesses);
-      numGuesses--;
+      alphabetObj[key].guessed = true;
+      if(randomWord.indexOf(key) > -1){
+        alphabetObj[key].correct = true;
+      }else{
+        numGuesses--;
+      }
       updateIndicator();
-      outputToBoard( randomWord, guesses );
+      outputToBoard();
+      if(checkWin()){
+        winScreen.style.top = '10%';
+      }
+      if(checkLose()){
+        loseScreen.style.top = '10%';
+      }
     }else{
       console.log('not a valid letter');
     }
   }
   window.addEventListener('keypress', handleKey);
+  winScreen.addEventListener('click', initializeGame);
+  loseScreen.addEventListener('click', initializeGame);
+  initializeGame();
 }());
